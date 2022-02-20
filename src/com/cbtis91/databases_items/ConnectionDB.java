00bdfd59d1ff24fe1b0e888575f1e8a3995eb101
José Castellanos <jotacastellanosguz@gmail.com>
2022-02-19 22:18:37 -0600

    Logger para depuración de errores y alerta de aviso al no poderse conectar a la base de datos

--- src/com/cbtis91/databases_items/ConnectionDB.java
+++ src/com/cbtis91/databases_items/ConnectionDB.java
@@ -2,19 +2,26 @@ package com.cbtis91.databases_items;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+
+import lombok.Data;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+@Data
 public class ConnectionDB {
 	    public static ConnectionDB conexionDB=null;
-	    private static final Logger logger= Logger.getLogger(ConnectionDB.class.getName());  
+	    private static final Logger logger= Logger.getLogger(ConnectionDB.class.getName());
+	    private Connection con=null;
 	    private ConnectionDB(){
-	    	Connection con=null;
 	    	try {
 	            Class.forName("com.mysql.cj.jdbc.Driver");  
 	            con= DriverManager.getConnection("jdbc:mysql://localhost:3306/servicios_escolares", "cbtis", "12345");
 			} catch (Exception e) {
-				logger.log(Level.WARNING, "Error {0}",e);
+				logger.log(Level.WARNING, "Error al conectar la base de datos {0}",e);
+				JOptionPane.showMessageDialog(null, "Error al conectar a la base de datos", "Error", 2);
 			}
 	    }
 	    
