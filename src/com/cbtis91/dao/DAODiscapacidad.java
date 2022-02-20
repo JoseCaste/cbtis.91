0ec4192e5a75a51639e9ff205cd336cff50a4377
José Castellanos <jotacastellanosguz@gmail.com>
2022-02-19 22:17:19 -0600

    Datos totales de tablas

--- src/com/cbtis91/dao/DAODiscapacidad.java
+++ src/com/cbtis91/dao/DAODiscapacidad.java
@@ -1,12 +1,33 @@
 package com.cbtis91.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import com.cbtis91.databases_items.ConnectionDB;
 import com.cbtis91.interfaces.IDAOcrud;
 import com.cbtis91.models.Discapacidad;
 
 public class DAODiscapacidad implements IDAOcrud<Discapacidad> {
 
+	private ConnectionDB connectionDB;
+	private Statement statement;
+	private ResultSet resultSet;
+	private static final Logger logger= Logger.getLogger(DAODiscapacidad.class.getName());
+	
+	public DAODiscapacidad() {
+		this.connectionDB=ConnectionDB.getInstance();
+		try {
+			this.statement= this.connectionDB.getCon().createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			logger.log(Level.WARNING, "Error en crear la intancia Statament en DAODiscapacidad {0}", e);
+		}
+	}
 	@Override
 	public boolean save(Discapacidad data) {
 		// TODO Auto-generated method stub
@@ -15,8 +36,18 @@ public class DAODiscapacidad implements IDAOcrud<Discapacidad> {
 
 	@Override
 	public List<Discapacidad> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Discapacidad> discapacidades= new ArrayList<>();
+		try {
+			this.resultSet= this.statement.executeQuery("Select * from discapacidad d");
+			while(this.resultSet.next()) {
+				discapacidades.add(new Discapacidad(this.resultSet.getInt("id_discapacidad"), this.resultSet.getString("nombre_discapacidad")));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			logger.log(Level.WARNING, "Error al recuperar datos de la tabla discapacidad {0}", e);
+			
+		}
+		return discapacidades;
 	}
 
 	@Override
--- src/com/cbtis91/dao/DAOLengua.java
+++ src/com/cbtis91/dao/DAOLengua.java
@@ -1,12 +1,32 @@
 package com.cbtis91.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import com.cbtis91.databases_items.ConnectionDB;
 import com.cbtis91.interfaces.IDAOcrud;
 import com.cbtis91.models.Lengua;
 
 public class DAOLengua implements IDAOcrud<Lengua> {
-
+	private ConnectionDB connectionDB;
+	private Statement statement;
+	private ResultSet resultSet;
+	private static final Logger logger= Logger.getLogger(DAOLengua.class.getName());
+	
+	public DAOLengua() {
+		this.connectionDB= ConnectionDB.getInstance();
+		try {
+			this.statement= this.connectionDB.getCon().createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			logger.log(Level.WARNING, "Error en crear la intancia Statament en DAOLengua{0}", e);
+		}
+	}
 	@Override
 	public boolean save(Lengua data) {
 		// TODO Auto-generated method stub
@@ -15,8 +35,18 @@ public class DAOLengua implements IDAOcrud<Lengua> {
 
 	@Override
 	public List<Lengua> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Lengua> languajes= new ArrayList<>();
+		try {
+			this.resultSet= this.statement.executeQuery("Select * from lengua u");
+			while(this.resultSet.next()) {
+				languajes.add(new Lengua(this.resultSet.getInt("id_lengua"), this.resultSet.getString("nombre_lengua")));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			logger.log(Level.WARNING, "Error al recuperar datos de la tabla lengua {0}", e);
+			
+		}
+		return languajes;
 	}
 
 	@Override
