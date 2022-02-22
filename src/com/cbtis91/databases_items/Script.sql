f9e358da66b04fb8e8d41420138606f36d24af24
José Castellanos <jotacastellanosguz@gmail.com>
2022-02-22 17:22:24 -0600

    Scripts de base de datos

---
+++ src/com/cbtis91/databases_items/Script.sql
@@ -0,0 +1,46 @@
+use servicios_escolares;
+
+SELECT * FROM ficha f ;
+describe ficha ;
+
+select * from especialidad e ;
+SELECT * from lengua l ;
+SELECT * from discapacidad d ;
+SELECT * from localidades l ;
+SELECT * from numeroFicha nf ;
+update numeroFicha set numero_ficha=1 where id_numero_ficha =1;
+insert into lengua (nombre_lengua) values ('Ninguno');
+SET @numFicha := (SELECT numero_ficha from numeroFicha nf)+1;
+SELECT  @numFicha;
+DELIMITER //
+CREATE PROCEDURE saveFicha(IN nombres_ VARCHAR(200),IN apellido_paterno_ varchar(200), IN apellido_materno_ varchar(200),IN age_ int,
+IN curp_ varchar(18),IN fk_id_localidad_ mediumint, IN address varchar(200),IN birth_place_ varchar(200),IN numero_telefono_ varchar(10),
+IN correo_electronico_ text, IN op1_especialidad_ mediumint, IN op2_especialidad_ mediumint,IN fk_id_lengua_ mediumint, IN fk_id_discapacidad_ mediumint, 
+IN kind_school_ varchar(200), OUT numeroFicha_ int)
+BEGIN
+	
+	INSERT INTO ficha (numero_ficha, nombres, apellido_paterno, apellido_materno, curp, numero_telefono, correo_electronico, op1_especialidad, 
+	op2_especialidad, fk_id_lengua, fk_id_discapacidad, kind_school, edad, fk_id_localidad, birth_place, direccion) values 
+	((SELECT numero_ficha from numeroFicha nf),nombres_,apellido_paterno_,apellido_materno_, curp_ , numero_telefono_ , correo_electronico_ ,op1_especialidad_ ,
+	op2_especialidad_ ,fk_id_lengua_ ,fk_id_discapacidad_ ,kind_school_,age_,fk_id_localidad_,birth_place_,address);
+	
+	set numeroFicha_ = (SELECT numero_ficha from numeroFicha nf);
+	update numeroFicha nf set nf.numero_ficha =nf.numero_ficha +1 where id_numero_ficha =1;
+END //
+	
+set @flag=true;
+SELECT  @flag;
+
+call saveFicha ('jose','castellanos','guzman',15,'ABV121',1,'Pescadores','Tehuantepec','9711533508','abc@gmail.com',1,2,4,4,'General',@numFicha);
+
+INSERT INTO ficha (numero_ficha, nombres, apellido_paterno, apellido_materno, curp, numero_telefono, correo_electronico, op1_especialidad, 
+	op2_especialidad, fk_id_lengua, fk_id_discapacidad, kind_school, edad, fk_id_localidad, birth_place, direccion) values 
+	((SELECT numero_ficha from numeroFicha nf),'jose','castellanos','guzman','ABV121','9711533508','abc@gmail.com',1,2,4,4,'General',15,1,'Tehuantepec','Pescadores');
+
+update numeroFicha nf set nf.numero_ficha =nf.numero_ficha +1 where id_numero_ficha =1;
+SELECT numero_ficha from numeroFicha nf;
+
+insert into localidades (nombre_localidad) values('CD. IXTEPEC'),('IXTALTEPEC'),('EL ESPINAL'),('H. CD. DE JUCHITÁN'),('LA VENTOSA'),('LA VENTA'),('UNIÓN HIDALGO'),('STO. DOMINGO INGENIO'),
+('STGO IXTALTEPEC'),('SANTA MARÍA XADANI'),('CHIHUITÁN'),('LAOLLAGA'),('TLACOTEPEC'),('TEHUANTEPEC'),('COMITANCILLO'),('SANTA MARÍA GUIENAGATI'),('GUEVEA DE HUMBULT'),('LACHIVIZA'),('GUICHIXÚ');
+
+DELETE from ficha where id_ficha =4;
