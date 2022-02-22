c7d79c77b49f3f2693651da3a9f9e957c8f0fdf1
José Castellanos <jotacastellanosguz@gmail.com>
2022-02-22 17:35:38 -0600

    Pruebas con Itext

--- src/com/cbtis91/controller/ItextTest.java
+++ src/com/cbtis91/controller/ItextTest.java
@@ -7,14 +7,18 @@ import java.net.MalformedURLException;
 import com.itextpdf.io.font.constants.StandardFonts;
 import com.itextpdf.io.image.ImageData;
 import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.colors.Color;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.canvas.draw.SolidLine;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.borders.Border;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.element.LineSeparator;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.property.TextAlignment;
@@ -36,11 +40,11 @@ public class ItextTest {
 	        PdfDocument pdf = new PdfDocument(writer);
 	        
 	        Document document = new Document(pdf);
-	        ImageData image_izq= ImageDataFactory.create("/home/jose/Pictures/logo_cbtis_izq.jpg");
+	        /*ImageData image_izq= ImageDataFactory.create("/home/jose/Pictures/logo_cbtis_izq.jpg");
 	        Image image= new Image(image_izq);
 	        image.scale(0.2f, 0.2f);
 	        image.setFixedPosition(10, 760);
-	        document.add(image);
+	        document.add(image);*/
 	        /*Table table = new Table(UnitValue.createPercentArray(new float[]{1, 2}));
 	        table.addCell(createImageCell("/home/jose/Pictures/logo_cbtis_izq.jpg"));
 	        table.addCell(createImageCell2("/home/jose/Pictures/logo_cbtis_der.jpg"));
@@ -75,12 +79,59 @@ public class ItextTest {
 	        
 	        document.add(new Paragraph("NOTA: Nota de prueba ").setTextAlignment(TextAlignment.JUSTIFIED).setFontSize(9).setPaddingLeft(48));
 	        
+	        document.add(new Paragraph(" "));
+	        
+	        drawLine(document);
+	        
+	        document.add(new Paragraph(" "));
+	        document.add(new Paragraph(" "));
+	        document.add(new Paragraph(" "));
+	        document.add(new Paragraph(" "));
+	        
+	        document.add(new Paragraph("SECRETARIA DE EDUCACIÓN PÚBLICA").setTextAlignment(TextAlignment.CENTER).setFont(font).setFontSize(9).setFixedLeading(1));
+	        document.add(new Paragraph("SUBSECRETARÍA DE EDUCACIÓN MEDIA SUPERIOR").setTextAlignment(TextAlignment.CENTER).setFont(font).setFontSize(9));
+	        document.add(new Paragraph("DIRECCIÓN GENERAL DE EDUCACIÓN TECNOLÓGICA industrial y de servicios").setFont(font).setTextAlignment(TextAlignment.CENTER).setFontSize(9).setFixedLeading(1));
+	        document.add(new Paragraph("CENTRO DE BACHILLERATO TECNOLÓGICO industrial y de servicios No. 91").setFont(font).setTextAlignment(TextAlignment.CENTER).setFontSize(9));
+	        
+	        document.add(new Paragraph(" "));
+	        
+	        document.add(new Paragraph("FICHA DE SOLICITUD DE EXAMEN DE INGRESO").setFont(font).setTextAlignment(TextAlignment.CENTER).setFontSize(9).setUnderline());
+
+	        document.add(new Paragraph(" "));
+	        
+	        document.add(new Paragraph("No. de FICHA: "+"5").setTextAlignment(TextAlignment.RIGHT).setFont(font).setFontSize(9));
+	        document.add(new Paragraph(" "));
+	        document.add(new Paragraph(" "));
+	        document.add(new Paragraph(" "));
+	        document.add(new Paragraph(" "));
+	        document.add(new Paragraph("NOMBRE DEL ALUMNO: Castellanos Guzmán Juan José").setTextAlignment(TextAlignment.JUSTIFIED).setFontSize(9).setPaddingLeft(48));
+	        document.add(new Paragraph("CURP: 1123CURP").setTextAlignment(TextAlignment.JUSTIFIED).setFontSize(9).setPaddingLeft(48));
+	        document.add(new Paragraph("TELEFONO: 9712212212").setTextAlignment(TextAlignment.JUSTIFIED).setFontSize(9).setPaddingLeft(48));
+	        document.add(new Paragraph("CORREO ELECTRÓNICO: ABC@gmail.com").setTextAlignment(TextAlignment.JUSTIFIED).setFontSize(9).setPaddingLeft(48));
+	        document.add(new Paragraph("LUGAR DE NACIEMIENTO: Aquí").setTextAlignment(TextAlignment.JUSTIFIED).setFontSize(9).setPaddingLeft(48));
+	        document.add(new Paragraph("OPCION 1 DE ESPECIALIDAD : 1").setTextAlignment(TextAlignment.JUSTIFIED).setFontSize(9).setPaddingLeft(48));
+	        document.add(new Paragraph("OPCION 2 DE ESPECIALIDAD : 2").setTextAlignment(TextAlignment.JUSTIFIED).setFontSize(9).setPaddingLeft(48));
+	        
+	        document.add(new Paragraph(" "));
+	        
+	        document.add(new Paragraph("NOTA: Nota de prueba ").setTextAlignment(TextAlignment.JUSTIFIED).setFontSize(9).setPaddingLeft(48));
+	        
+	        
 	        document.close();
 			System.out.println("PDF creado");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+	}
+	private static void drawLine(Document document) {
+		SolidLine line = new SolidLine(1f);
+		//line.setColor(P);
+		LineSeparator ls = new LineSeparator(line);
+		ls.setWidth(500);
+		ls.setMarginTop(5);
+		document.add(ls);
 		
 	}
 	public static Cell createImageCell(String path) throws MalformedURLException {
