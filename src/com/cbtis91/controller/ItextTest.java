83468d3d706f38de7ad5db1e95a285015b6ce40e
José Castellanos <jotacastellanosguz@gmail.com>
2022-02-22 12:51:32 -0600

    Agregación de imagenes en posición absoluta

--- src/com/cbtis91/controller/ItextTest.java
+++ src/com/cbtis91/controller/ItextTest.java
@@ -2,14 +2,24 @@ package com.cbtis91.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+
 import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.borders.Border;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.UnitValue;
+import com.itextpdf.layout.property.VerticalAlignment;
 
 public class ItextTest {
 
@@ -26,7 +36,15 @@ public class ItextTest {
 	        PdfDocument pdf = new PdfDocument(writer);
 	        
 	        Document document = new Document(pdf);
-	        
+	        ImageData image_izq= ImageDataFactory.create("/home/jose/Pictures/logo_cbtis_izq.jpg");
+	        Image image= new Image(image_izq);
+	        image.scale(0.2f, 0.2f);
+	        image.setFixedPosition(10, 760);
+	        document.add(image);
+	        /*Table table = new Table(UnitValue.createPercentArray(new float[]{1, 2}));
+	        table.addCell(createImageCell("/home/jose/Pictures/logo_cbtis_izq.jpg"));
+	        table.addCell(createImageCell2("/home/jose/Pictures/logo_cbtis_der.jpg"));
+	        document.add(table);*/
 	      
 	        PdfFont font= PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);
 	        document.add(new Paragraph("SECRETARIA DE EDUCACIÓN PÚBLICA").setTextAlignment(TextAlignment.CENTER).setFont(font).setFontSize(9).setFixedLeading(1));
@@ -64,6 +82,22 @@ public class ItextTest {
 			e.printStackTrace();
 		}
 		
+	}
+	public static Cell createImageCell(String path) throws MalformedURLException {
+	    Image img = new Image(ImageDataFactory.create(path));
+	    img.setFixedPosition(2f, 1f);
+	    img.setWidth(UnitValue.createPercentValue(20));
+	    Cell cell = new Cell().add(img);
+	    cell.setBorder(null);
+	    return cell;
+	}
+
+	public static Cell createImageCell2(String path) throws MalformedURLException {
+		Image img = new Image(ImageDataFactory.create(path));
+	    img.setWidth(UnitValue.createPercentValue(20));
+	    Cell cell = new Cell().add(img).setTextAlignment(TextAlignment.RIGHT);
+	    cell.setBorder(null);
+	    return cell;
 	}
 
 }
